1e44e196b2b8f07f66ff1b9a763bac0a8c8e7c77
WebWaka Agent 1 <webwaka.agent.1@gmail.com>
2026-01-27 20:04:44 -0500

    fix: include node_modules in Lambda deployment package

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -39,7 +39,7 @@ data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../backend"
   output_path = "${path.module}/../backend.zip"
-  excludes    = ["node_modules", ".git", ".gitignore"]
+  excludes    = [".git", ".gitignore"]
 }
 
 # Lambda Function
